5097d8b63b3248e4ace041bb0957b732b613d01a
Nobert-Muma <mumanobert17@gmail.com>
2024-06-25 00:39:23 +0300

    Using Math mehods

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+console.log(multiply);
+
+let random=Math.floor(Math.random()*10)+2;
+Number.isInteger(random);
+console.log(random);
+
+let num3=25;
+let num4=7;
+let mod=num3%num4;
+console.log(mod);
+
+let max=Math.max(12, 9, 13, 8, 20);
+console.log(max);
+
+
